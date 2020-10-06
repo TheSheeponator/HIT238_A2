7029e9a152c60f1bbde82d5c7a2120679f1dde85
TheSheeponator <swhume12@gmail.com>
2020-10-06 20:46:33 +0930

    Testing Login.inc.php as a pass through This is an attempt to assign a Cookie with the server, while useing the Pages for content. Not sure if it will work...

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -20,68 +20,73 @@ session_start();
  // Base code by (Insert name here), modified by Sean Hume, further modified for ajax use.
  if (isset($_POST['loginsubmit'])) {
 
-    require 'dbh.inc.php';
-    // require '../Redirect.php';
+  require 'dbh.inc.php';
+  // require '../Redirect.php';
 
-    $mailuid = $_POST['uid'];
-    $password = $_POST['pwd'];
+  $mailuid = $_POST['uid'];
+  $password = $_POST['pwd'];
 
-    $return_arr_json = array();
+  $return_arr_json = array();
 
-    if (empty($mailuid) || empty($password)) {
-        $return_arr_json = array("error" => "emptyfields");
-        echo json_encode($return_arr_json);
+  if (empty($mailuid) || empty($password)) {
+    // $return_arr_json = array("error" => "emptyfields");
+    // echo json_encode($return_arr_json);
+    header("Location: https://thesheeponator.github.io/HIT238_A2?error=emptyfields&uid=".$mailuid);
+    exit(); //Used to ensure the stop of code execution
+  }
+  else {
+    $sql = "SELECT * FROM sysusers WHERE uidUsers=? OR emailUsers=?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      //  $return_arr_json = array("error" => "internalerror");
+      //  echo json_encode($return_arr_json);
+      header("Location: https://thesheeponator.github.io/HIT238_A2?error=internalerror");
+      exit(); //Used to ensure the stop of code execution
+    } else {
+      mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      if ($row = mysqli_fetch_assoc($result)) {
+        $pwdCheck = password_verify($password, $row['pwdUsers']);
+        if ($pwdCheck == false) {
+          // $return_arr_json = array("error" => "incorrectcredentials");
+          // echo json_encode($return_arr_json);
+          header("Location: https://thesheeponator.github.io/HIT238_A2?error=wrongPU&uid=".$mailuid);
+          exit(); //Used to ensure the stop of code execution
+        }
+        else if ($pwdCheck == true)
+        {
+          session_start();
+          session_regenerate_id(true);
+          $_SESSION['userId'] = $row['idUsers'];
+          $_SESSION['userUid'] = $row['uidUsers'];
+
+          $_SESSION['LAST_ACTIVITY'] = time();
+          
+          // $return_arr_json = array("success" => "./control");
+          // echo json_encode($return_arr_json);
+          header("Location: https://thesheeponator.github.io/HIT238_A2/control");
+          exit(); //Used to ensure the stop of code execution
+          //}
+        }
+        else {
+          // $return_arr_json = array("error" => "incorrectcredentials");
+          // echo json_encode($return_arr_json);
+          header("Location: https://thesheeponator.github.io/HIT238_A2?error=wrongPU&uid=".$mailuid);
+          exit(); //Used to ensure the stop of code execution
+        }
+      }
+      else {
+        // $return_arr_json = array("error" => "incorrectcredentials");
+        // echo json_encode($return_arr_json);
+        header("Location: https://thesheeponator.github.io/HIT238_A2?error=wrongPU&uid=".$mailuid);
         exit(); //Used to ensure the stop of code execution
+      }
     }
-    else {
-       $sql = "SELECT * FROM sysusers WHERE uidUsers=? OR emailUsers=?;";
-       $stmt = mysqli_stmt_init($conn);
-       if (!mysqli_stmt_prepare($stmt, $sql)) {
-         $return_arr_json = array("error" => "internalerror");
-         echo json_encode($return_arr_json);
-         exit(); //Used to ensure the stop of code execution
-       } else {
-
-         mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
-         mysqli_stmt_execute($stmt);
-         $result = mysqli_stmt_get_result($stmt);
-         if ($row = mysqli_fetch_assoc($result)) {
-           $pwdCheck = password_verify($password, $row['pwdUsers']);
-           if ($pwdCheck == false)
-           {
-            $return_arr_json = array("error" => "incorrectcredentials");
-            echo json_encode($return_arr_json);
-            exit(); //Used to ensure the stop of code execution
-           }
-           else if ($pwdCheck == true)
-           {
-              session_start();
-              session_regenerate_id(true);
-              $_SESSION['userId'] = $row['idUsers'];
-              $_SESSION['userUid'] = $row['uidUsers'];
-
-              $_SESSION['LAST_ACTIVITY'] = time();
-              
-              $return_arr_json = array("success" => "./control");
-              echo json_encode($return_arr_json);
-              exit(); //Used to ensure the stop of code execution
-              //}
-           }
-           else {
-              $return_arr_json = array("error" => "incorrectcredentials");
-              echo json_encode($return_arr_json);
-              exit(); //Used to ensure the stop of code execution
-           }
-         }
-         else {
-            $return_arr_json = array("error" => "incorrectcredentials");
-            echo json_encode($return_arr_json);
-            exit(); //Used to ensure the stop of code execution
-         }
-       }
-    }
+  }
  }
  else {
-    $return_arr_json = array("error" => "emptyfields");
-    echo json_encode($return_arr_json);
+//     $return_arr_json = array("error" => "emptyfields");
+//     echo json_encode($return_arr_json);
+    header("Location: https://thesheeponator.github.io/HIT238_A2?error=emptyfields&uid=".$mailuid);
  }
