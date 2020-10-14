2cb4df58837da980c59432e5af2484a98be8e07a
TheSheeponator <swhume12@gmail.com>
2020-10-14 11:23:31 +0930

    Added function to chack for valid permissions

--- API/session.php
+++ API/session.php
@@ -4,7 +4,20 @@ function newSession($conn, $userid) {
     $newSessionID = hash('sha256', random_int(50, 9999));
     $newSessionTimeOut = new DateTime();
     $newSessionTimeOut->add(new DateInterval('PT10M'));
-    $sessionSQL = "UPDATE sysusers SET sessionID='".$newSessionID."', sessionTimeOut='".$newSessionTimeOut->format('Y-m-d H:i:s')."' WHERE idusers = '1'";
+    $sessionSQL = "UPDATE sysusers SET sessionID='".$newSessionID."', sessionTimeOut='".$newSessionTimeOut->format('Y-m-d H:i:s')."' WHERE idusers = '".$userid."'";
+
+    $sessionResult = mysqli_query($conn, $sessionSQL);
+    if ($sessionResult) {
+        return $newSessionID;
+    } else {
+        return false;
+    }
+}
+
+function endSession($conn, $apiID) {
+    $newSessionID = null;
+    $newSessionTimeOut = new DateTime();
+    $sessionSQL = "UPDATE sysusers SET sessionID='".$newSessionID."', sessionTimeOut='".$newSessionTimeOut->format('Y-m-d H:i:s')."' WHERE sessionID = '".$apiID."'";
 
     $sessionResult = mysqli_query($conn, $sessionSQL);
     if ($sessionResult) {
@@ -40,13 +53,29 @@ function checkSession($conn, $apiID) {
         $resultCheck = mysqli_stmt_num_rows($stmt);
         mysqli_stmt_bind_result($stmt, $sessionTimeOut);
         mysqli_stmt_close($stmt);
-        if ($resultCheck <= 0) {
+        if ($resultCheck > 0) {
             $date = date_create_from_format('Y-m-d H:i:s', $sessionTimeOut);
             $now = new DateTime();
             $diff = date_diff($now, $date, true);
             if (intval(date_format($diff, "i"), 10) < 10 && intval(date_format($diff, "i"), 10) > 0) {
                 return true;
             }
+        }
+    return false;
+    }
+}
+
+function CheckPerm($conn, $apiID) {
+    $sql = "SELECT `idUsers` FROM `sysusers` WHERE `sessionID` = ? AND `Perms` = 1";
+    $stmt = mysqli_stmt_init($conn);
+    if (mysqli_stmt_prepare($stmt, $sql)) {
+        mysqli_stmt_bind_param($stmt, 's', $apiID);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        $resultCheck = mysqli_stmt_num_rows($stmt);
+        mysqli_stmt_close($stmt);
+        if ($resultCheck <= 0) {
+            return true;
         }
     return false;
     }
